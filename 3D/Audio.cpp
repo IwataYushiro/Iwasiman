c41ca6654db842dd8e4a60a9ae185a95d795dd35
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-01-05 02:02:43 +0900

    初期化と各サブクラス(構造体)を定義

--- 3D/Audio.cpp
+++ 3D/Audio.cpp
@@ -2,5 +2,12 @@
 
 void Audio::Initialize()
 {
+	HRESULT result;
+
+	//XAudioのインスタンスを生成
+	result = XAudio2Create(&xaudio2, 0, XAUDIO2_DEFAULT_PROCESSOR);
+
+	//マスターボイスを生成
+	result = xaudio2->CreateMasteringVoice(&masterVoice);
 
 }
--- 3D/Audio.h
+++ 3D/Audio.h
@@ -11,6 +11,39 @@ private://エイリアス
 	// Microsoft::WRL::を省略
 	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 
+private://サブクラス
+	//チャンクヘッダ
+	struct ChunkHeader
+	{
+		char id[4];		//チャンク用のID
+		int32_t size;	//チャンクサイズ
+	};
+
+	//RIFFヘッダチャンク
+	struct RiffHeader
+	{
+		ChunkHeader chunk;	//"RIFF"
+		char type[4];		//"WAVE"
+	};
+
+	//Formatチャンク
+	struct FormatChunk
+	{
+		ChunkHeader chunk;	//"fmt"
+		WAVEFORMATEX fmt;	//波形フォーマット
+	};
+
+	//サウンドデータ
+	struct SoundData
+	{
+		//波形フォーマット
+		WAVEFORMATEX wfex;
+		//バッファの先頭アドレス
+		BYTE* pBuffer;
+		//バッファのサイズ
+		unsigned int bufferSize;
+	};
+
 public://メンバ関数
 	
 	//初期化
