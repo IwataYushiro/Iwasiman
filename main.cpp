379dabb4912a37504fff63ae41738b5d41108263
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-01-10 15:33:57 +0900

    MyFirstTool関数化

--- main.cpp
+++ main.cpp
@@ -24,6 +24,42 @@ void ImGuiStyleShowSample() {
 	ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
 }
 
+void ImGuiMyFirstToolColor()
+{
+	// Create a window called "My First Tool", with a menu bar.
+	static bool my_tool_active = true;
+	static float my_color[4] = { 0.0f,0.0f,0.0f,0.0f };
+
+	ImGui::Begin("My First Tool", &my_tool_active, ImGuiWindowFlags_MenuBar);
+	if (ImGui::BeginMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+			if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
+			if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
+			ImGui::EndMenu();
+		}
+		ImGui::EndMenuBar();
+	}
+
+	// Edit a color stored as 4 floats
+	ImGui::ColorEdit4("Color", my_color);
+
+	// Generate samples and plot them
+	float samples[100];
+	for (int n = 0; n < 100; n++)
+		samples[n] = sinf(n * 0.2f + ImGui::GetTime() * 1.5f);
+	ImGui::PlotLines("Samples", samples, 100);
+
+	// Display contents in a scrolling region
+	ImGui::TextColored(ImVec4(1, 1, 0, 1), "Important Stuff");
+	ImGui::BeginChild("Scrolling");
+	for (int n = 0; n < 50; n++)
+		ImGui::Text("%04d: Some text", n);
+	ImGui::EndChild();
+	ImGui::End();
+}
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
@@ -114,39 +150,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		//ImGui呼び出し
 		imguiManager->Begin();
 		//ここからImGuiの表示項目を追加する
-		// Create a window called "My First Tool", with a menu bar.
-		static bool my_tool_active = true;
-		static float my_color[4] = { 0.0f,0.0f,0.0f,0.0f };
-
-		ImGui::Begin("My First Tool", &my_tool_active, ImGuiWindowFlags_MenuBar);
-		if (ImGui::BeginMenuBar())
-		{
-			if (ImGui::BeginMenu("File"))
-			{
-				if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
-				if (ImGui::MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
-				if (ImGui::MenuItem("Close", "Ctrl+W")) { my_tool_active = false; }
-				ImGui::EndMenu();
-			}
-			ImGui::EndMenuBar();
-		}
-
-		// Edit a color stored as 4 floats
-		ImGui::ColorEdit4("Color", my_color);
-
-		// Generate samples and plot them
-		float samples[100];
-		for (int n = 0; n < 100; n++)
-			samples[n] = sinf(n * 0.2f + ImGui::GetTime() * 1.5f);
-		ImGui::PlotLines("Samples", samples, 100);
-
-		// Display contents in a scrolling region
-		ImGui::TextColored(ImVec4(1, 1, 0, 1), "Important Stuff");
-		ImGui::BeginChild("Scrolling");
-		for (int n = 0; n < 50; n++)
-			ImGui::Text("%04d: Some text", n);
-		ImGui::EndChild();
-		ImGui::End();
+		ImGuiMyFirstToolColor();
 		//表示項目ここまで
 		imguiManager->End();
 
