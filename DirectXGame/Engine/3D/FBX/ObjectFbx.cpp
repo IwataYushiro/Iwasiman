57fc445dbdbfb38cdd5e3744d631614b947d5335
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-05-07 01:03:05 +0900

    定数バッファを生成

--- DirectXGame/Engine/3D/FBX/ObjectFbx.cpp
+++ DirectXGame/Engine/3D/FBX/ObjectFbx.cpp
@@ -3,3 +3,26 @@
 //静的メンバ変数の実体
 ID3D12Device* ObjectFbx::device_ = nullptr;
 Camera* ObjectFbx::camera_ = nullptr;
+
+void ObjectFbx::Initialize()
+{
+	HRESULT result;
+
+	// ヒーププロパティ
+	CD3DX12_HEAP_PROPERTIES heapProps = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+	// リソース設定
+	CD3DX12_RESOURCE_DESC resourceDesc =
+		CD3DX12_RESOURCE_DESC::Buffer((sizeof(ConstBufferDataTransform) + 0xff) & ~0xff);
+
+	HRESULT result;
+
+	// 定数バッファの生成
+	result = device_->CreateCommittedResource(
+		&heapProps, // アップロード可能
+		D3D12_HEAP_FLAG_NONE,
+		&resourceDesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&constBufferTransform));
+
+}
--- DirectXGame/Engine/3D/FBX/ObjectFbx.h
+++ DirectXGame/Engine/3D/FBX/ObjectFbx.h
@@ -20,15 +20,33 @@ protected://エイリアス
 	using XMFLOAT4 = DirectX::XMFLOAT4;
 	using XMMATRIX = DirectX::XMMATRIX;
 
+public://サブクラス
+	//定数バッファ構造体(座標変換行列)
+	struct ConstBufferDataTransform
+	{
+		XMMATRIX viewProj;		//ビュプロ行列
+		XMMATRIX world;			//ワールド行列
+		XMFLOAT3 cameraPos;		//カメラ座標
+	};
+
 public://静的メンバ関数
 	//セッター
 	static void SetDevice(ID3D12Device* device) { ObjectFbx::device_ = device; }
 	static void SetCamera(Camera* camera) { ObjectFbx::camera_ = camera; }
-
 
 private://静的メンバ変数
 	//デバイス
 	static ID3D12Device* device_;
 	//カメラ
 	static Camera* camera_;
+
+public://メンバ関数
+
+	//初期化
+	void Initialize();
+
+protected://メンバ変数
+	//定数バッファ
+	ComPtr<ID3D12Resource> constBufferTransform;
+
 };
