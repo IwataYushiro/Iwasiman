de0bac2df6277d3ad5c56d5ffb59863d03b4ab77
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-06-01 11:48:45 +0900

    イメージデータ転送まで

--- DirectXGame/Engine/2D/PostEffect/PostEffect.cpp
+++ DirectXGame/Engine/2D/PostEffect/PostEffect.cpp
@@ -1,10 +1,50 @@
 #include "PostEffect.h"
 #include <d3dx12.h>
+#include "WinApp.h"
 
 using namespace DirectX;
 
 PostEffect::PostEffect()
 {
+}
+
+void PostEffect::Initialize()
+{
+	HRESULT result;
+	//基盤クラスとしての初期化
+	Sprite::Initialize(spCommon_);
+	//テクスチャリソース設定
+	CD3DX12_RESOURCE_DESC texresDesc = CD3DX12_RESOURCE_DESC::Tex2D(
+		DXGI_FORMAT_B8G8R8X8_UNORM_SRGB,
+		WinApp::window_width,
+		(UINT)WinApp::window_height,
+		1, 0, 1, 0, D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET);
+	
+	//テクスチャバッファ生成
+	result = spCommon_->GetDxCommon()->GetDevice()->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_CPU_PAGE_PROPERTY_WRITE_BACK, D3D12_MEMORY_POOL_L0),
+		D3D12_HEAP_FLAG_NONE,
+		&texresDesc,
+		D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+		nullptr,
+		IID_PPV_ARGS(&texBuff));
+	assert(SUCCEEDED(result));
+
+	//テクスチャ赤クリア
+	//画素数(1280 x 720 = 921600ピクセル)
+	const UINT pixelCount = WinApp::window_width * WinApp::window_height;
+	//画像一枚分のデータサイズ
+	const UINT rowPitch = sizeof(UINT) * WinApp::window_width;
+	//画像全体のデータサイズ
+	const UINT depthPitch = rowPitch * WinApp::window_height;
+	//画像イメージ
+	UINT* img = new UINT[pixelCount];
+	for (int i = 0; i < pixelCount; i++) { img[i] = 0xff0000ff; }
+
+	//テクスチャバッファにデータ転送
+	result = texBuff->WriteToSubresource(0, nullptr, img, rowPitch, depthPitch);
+	assert(SUCCEEDED(result));
+	delete[] img;
 }
 
 void PostEffect::Draw(ID3D12GraphicsCommandList* cmdList)
--- DirectXGame/Engine/2D/PostEffect/PostEffect.h
+++ DirectXGame/Engine/2D/PostEffect/PostEffect.h
@@ -6,9 +6,15 @@ class PostEffect :  public Sprite
 public:
     //コンストラクタ
     PostEffect();
-
-
+    //初期化
+    void Initialize();
     //描画
     void Draw(ID3D12GraphicsCommandList* cmdList);
+
+private:
+    //テクスチャバッファ
+    ComPtr<ID3D12Resource> texBuff;
+    //SRV用デスクリプタヒープ
+    ComPtr<ID3D12DescriptorHeap> descHeapSRV;
 };
 
