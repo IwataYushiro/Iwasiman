141eca52dcc2ec3359aa83c3a9803f1a0c133a49
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-06-15 12:20:00 +0900

    色反転までよ

--- Resources/shader/PostEffect/TestPS.hlsl
+++ Resources/shader/PostEffect/TestPS.hlsl
@@ -6,6 +6,32 @@ SamplerState smp      : register(s0);	//0番スロットに設定されたサンプラー
 float4 main(VSOutput input) : SV_TARGET
 {
     float4 texcolor = tex.Sample(smp, input.uv);
-    return float4(texcolor.rgb, 1);
+    return float4(float3(1.0f, 1.0f, 1.0f) - texcolor.rgb, 1.0f);
 
 }
+
+/*
+ベース
+float4 main(VSOutput input) : SV_TARGET
+{
+    float4 texcolor = tex.Sample(smp, input.uv);
+    return float4(texcolor.rgb, 1.0f);
+
+}
+
+明度変更
+float4 main(VSOutput input) : SV_TARGET
+{
+    float4 texcolor = tex.Sample(smp, input.uv);
+     return float4(texcolor.rgb * 2.0f, 1.0f);
+
+}
+緑だけ変更
+float4 main(VSOutput input) : SV_TARGET
+{
+    float4 texcolor = tex.Sample(smp, input.uv);
+    return float4(texcolor.r, texcolor.g * 2.0f, texcolor.b, 1.0f);
+
+}
+
+*/
