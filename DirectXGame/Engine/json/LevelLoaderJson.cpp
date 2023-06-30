647425ad39d2570076565640f733814894b88f7b
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-06-30 15:58:06 +0900

    ローダーはできた

---
+++ DirectXGame/Engine/json/LevelLoaderJson.cpp
@@ -0,0 +1,85 @@
+#include "LevelLoaderJson.h"
+
+#include <json.hpp>
+#include <fstream>
+#include <cassert>
+
+const std::string LevelLoader::defaultBaseDirectory = "Resources/levels/";
+const std::string LevelLoader::extension = ".json";
+
+LevelData* LevelLoader::LoadFile(const std::string& fileName)
+{
+	//連結してフルパスを得る
+	const std::string fullpath = defaultBaseDirectory + fileName + extension;
+
+	// ファイルストリーム
+	std::ifstream file;
+
+	// ファイルを開く
+	file.open(fullpath);
+	// ファイルオープン失敗をチェック
+	if (file.fail()) {
+		assert(0);
+	}
+
+	//json文字列から解凍したデータ
+	nlohmann::json deserialized;
+	//解凍
+	file >> deserialized;
+
+	// 正しいレベルデータファイルかチェック
+	assert(deserialized.is_object());
+	assert(deserialized.contains("name"));
+	assert(deserialized["name"].is_string());
+
+	// "name"を文字列として取得
+	std::string name = deserialized["name"].get<std::string>();
+	// 正しいレベルデータファイルかチェック
+	assert(name.compare("scene") == 0);
+
+	//レベルデータ格納用インスタンス
+	LevelData* lvData = new LevelData();
+	// "objects"の全オブジェクトを走査
+	for (nlohmann::json& object : deserialized["objects"])
+	{
+		//typeが無ければ停止
+		assert(object.contains("type"));
+
+		// 種別を取得
+		std::string type = object["type"].get<std::string>();
+
+		// type=MESH
+		if (type.compare("MESH")==0)
+		{
+			lvData->objects.emplace_back(LevelData::ObjectData{});
+			// 今追加した要素の参照を得る
+			LevelData::ObjectData& objectData = lvData->objects.back();
+
+			if (object.contains("file_name")) {
+				// ファイル名
+				objectData.fileName = object["file_name"];
+			}
+
+			// トランスフォームのパラメータ読み込み
+			nlohmann::json& transform = object["transform"];
+			// 平行移動
+			objectData.trans.m128_f32[0] = (float)transform["translation"][1];
+			objectData.trans.m128_f32[1] = (float)transform["translation"][2];
+			objectData.trans.m128_f32[2] = -(float)transform["translation"][0];
+			objectData.trans.m128_f32[3] = 1.0f;
+			// 回転角
+			objectData.rot.m128_f32[0] = -(float)transform["rotation"][1];
+			objectData.rot.m128_f32[1] = -(float)transform["rotation"][2];
+			objectData.rot.m128_f32[2] = (float)transform["rotation"][0];
+			objectData.rot.m128_f32[3] = 0.0f;
+			// スケーリング
+			objectData.scale.m128_f32[0] = (float)transform["scaling"][1];
+			objectData.scale.m128_f32[1] = (float)transform["scaling"][2];
+			objectData.scale.m128_f32[2] = (float)transform["scaling"][0];
+			objectData.scale.m128_f32[3] = 0.0f;
+		}
+		//オブジェクト走査を再帰関数で走査
+	}
+
+	return nullptr;
+}
--- DirectXGame/Engine/json/LevelLoaderJson.h
+++ DirectXGame/Engine/json/LevelLoaderJson.h
@@ -1,1 +1,37 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <DirectXMath.h>
+
+//jsonファイルによるレベルデータ
+struct LevelData
+{
+	struct ObjectData
+	{
+		//ファイル名
+		std::string fileName;
+		//平行移動
+		DirectX::XMVECTOR trans;
+		//回転角
+		DirectX::XMVECTOR rot;
+		//スケール
+		DirectX::XMVECTOR scale;
+	};
+	//オブジェクト配列
+	std::vector<ObjectData> objects;
+};
+
+//レベルローダー
+class LevelLoader
+{
+public:
+	//デフォルトの読み込みディレクトリ
+	static const std::string defaultBaseDirectory;
+	//ファイル拡張子(json)
+	static const std::string extension;
+
+public:
+	// レベルデータファイルの読み込み
+	static LevelData* LoadFile(const std::string& fileName);
+
+};
