175925abd2f682cbe476f2a2d09398a52ef94b28
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-07-09 00:14:15 +0900

    コライダー製作準備

--- DirectXGame/Engine/3D/Light/LightGroup.cpp
+++ DirectXGame/Engine/3D/Light/LightGroup.cpp
@@ -125,15 +125,15 @@ void LightGroup::TransferConstBuffer()
 void LightGroup::DefaultLightSetting()
 {
 	dirLights[0].SetActive(true);
-	dirLights[0].SetLightColor({ 0.0f,0.0f,1.0f });
+	dirLights[0].SetLightColor({ 1.0f,1.0f,1.0f });
 	dirLights[0].SetLightDir({ 0.0f,-1.0f,0.0f,0.0f });
 
 	dirLights[1].SetActive(true);
-	dirLights[1].SetLightColor({ 0.0f,0.0f,1.0f });
+	dirLights[1].SetLightColor({ 1.0f,1.0f,1.0f });
 	dirLights[1].SetLightDir({ 0.5f,0.1f,0.2f,0.0f });
 
 	dirLights[2].SetActive(true);
-	dirLights[2].SetLightColor({ 0.0f,0.0f,1.0f });
+	dirLights[2].SetLightColor({ 1.0f,1.0f,1.0f });
 	dirLights[2].SetLightDir({ -0.5f,0.1f,-0.2f,0.0f });
 
 	pointLights[0].SetActive(false);
