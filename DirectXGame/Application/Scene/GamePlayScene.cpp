3490bb38912fdda5bc6ee5918146277ac9191744
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2023-08-22 20:09:14 +0900

    1面にも雑魚敵配置

--- DirectXGame/Application/Scene/GamePlayScene.cpp
+++ DirectXGame/Application/Scene/GamePlayScene.cpp
@@ -47,7 +47,7 @@ void GamePlayScene::Initialize()
 	//モデル読み込み
 	LoadModel();
 	//レベルデータ読み込み
-	if (stageNum == 1)LoadLVData("stage2");
+	if (stageNum == 1)LoadLVData("stage1");
 	else if (stageNum == 2)LoadLVData("stage2");
 
 	//ライトを生成
